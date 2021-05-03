e243e7c18e1d33f3d574f97ab64710872e44de4b
Paprikette <transnd@gmail.com>
2021-05-03 09:12:49 +0200

    Jquery Partie 3 Exo 5

---
+++ script.js
@@ -0,0 +1,28 @@
+$(function () {
+    $("input").focusin(function () {
+        $("body").keydown(function (e) {
+            switch (e.which) {
+                case 37: // flèche gauche
+                    $(".colorDiv").finish().animate({
+                        left: "-=10"
+                    });
+                    break;
+                case 38: // flèche haut
+                    $(".colorDiv").finish().animate({
+                        top: "-=10"
+                    });
+                    break;
+                case 39: // flèche droite
+                    $(".colorDiv").finish().animate({
+                        left: "+=10"
+                    });
+                    break;
+                case 40: // flèche du bas
+                    $(".colorDiv").finish().animate({
+                        top: "+=10"
+                    });
+                    break;
+            }
+        });
+    });
+})
